3d94003e805a0aea1362f7ec763deb76818efd28
Atticus Russell <atticurussell@gmail.com>
2023-04-04 21:41:01 -0400

    changed spawner.py to spawner to support ROS humble

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -33,54 +33,54 @@ def generate_launch_description():
 
     robot_controller_spawner1 = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joints1_controllers", "-c", "/controller_manager"],
     )
 
     robot_controller_spawner2 = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joints2_controllers", "-c", "/controller_manager"],
     )
 
     robot_controller_spawner3 = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joints3_controllers", "-c", "/controller_manager"],
     )
 
     robot_controller_spawner4 = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joints4_controllers", "-c", "/controller_manager"],
     )
 
     robot_controller_spawner5 = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joints5_controllers", "-c", "/controller_manager"],
     )
 
     robot_controller_spawner6 = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joints6_controllers", "-c", "/controller_manager"],
     )
     robot_controller_spawner7 = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joints7_controllers", "-c", "/controller_manager"],
     )
 
     robot_controller_spawner8 = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joints8_controllers", "-c", "/controller_manager"],
     )
 
     robot_controller_spawner9 = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joints9_controllers", "-c", "/controller_manager"],
     )
 
@@ -88,7 +88,7 @@ def generate_launch_description():
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
     )
 
